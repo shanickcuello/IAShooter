c7d3a884efc4c3ff9c3f80b4e1db01f3045d3a68
roland-vidar <shanlogauthier@gmail.com>
2022-05-08 22:32:25 -0300

    Tile creator wip

---
+++ Assets/Editor/GameObjectCreatorTool.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Editor
+{
+    public class GameObjectCreatorTool : MonoBehaviour
+    {
+        static void CreateGameObject(GameObject gameObject)
+        {
+            gameObject = new GameObject("New GameObject");
+            
+        }
+    }
+}
---
+++ Assets/Editor/PrefabCreatorWindow.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class PrefabCreatorWindow : EditorWindow
+    {
+        GameObject prefab;
+
+        [MenuItem("Window/Prefab Creator")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(PrefabCreatorWindow));
+        }
+
+
+        void OnGUI()
+        {
+            GUILayout.Label("Tile Creator", EditorStyles.boldLabel);
+            GUILayout.Space(50);
+            prefab = EditorGUI.ObjectField(new Rect(
+                    10,
+                    10,
+                    position.width - 20, 20),
+                "Tile", prefab, typeof(GameObject),
+                false) as GameObject;
+            //space 
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Create Prefab"))
+            {
+                if (prefab == null)
+                {
+                    Debug.Log("Pls put a prefab on the field");
+                    return;
+                }
+                CreatePrefabs();
+            }
+        }
+
+        private void CreatePrefabs()
+        {
+            var tileFather = new GameObject("TileContainer");
+            
+            var tile =  Instantiate(prefab);
+            tile.transform.position = Vector3.zero;
+
+        }
+    }
+}
