df7331bb84b4c9a18708e685c87121755e22fcad
roland-vidar <shanlogauthier@gmail.com>
2022-05-08 23:21:56 -0300

    tile tool creator done

--- Assets/Editor/PrefabCreatorWindow.cs
+++ Assets/Editor/PrefabCreatorWindow.cs
@@ -6,11 +6,15 @@ namespace Editor
     public class PrefabCreatorWindow : EditorWindow
     {
         GameObject prefab;
+        private float startPositionX, startPositionY, startPositionZ;
+        private int amountOfTilesX, amountOfTilesY;
 
         [MenuItem("Window/Prefab Creator")]
         public static void ShowWindow()
         {
-            GetWindow(typeof(PrefabCreatorWindow));
+            var window = GetWindow<PrefabCreatorWindow>();
+            window.position = new Rect(0, 0, 80, 30);
+            window.Show();
         }
 
 
@@ -20,20 +24,47 @@ namespace Editor
             GUILayout.Space(50);
             prefab = EditorGUI.ObjectField(new Rect(
                     10,
-                    10,
+                    20,
                     position.width - 20, 20),
                 "Tile", prefab, typeof(GameObject),
                 false) as GameObject;
             //space 
             GUILayout.Space(20);
 
-            if (GUILayout.Button("Create Prefab"))
+            startPositionX = EditorGUI.FloatField(new Rect(10, 50, 200, 20),
+                "Start position X",
+                startPositionX);
+
+            GUILayout.Space(20);
+
+
+            startPositionY = EditorGUI.FloatField(new Rect(10, 70, 200, 20),
+                "Start position y",
+                startPositionY);
+
+            startPositionZ = EditorGUI.FloatField(new Rect(10, 90, 200, 20),
+                "Start position z",
+                startPositionZ);
+
+            amountOfTilesX = EditorGUI.IntField(new Rect(10, 110, 200, 20),
+                "Amount tiles in X",
+                amountOfTilesX);
+
+            amountOfTilesY = EditorGUI.IntField(new Rect(10, 130, 200, 20),
+                "Amount tiles in Y",
+                amountOfTilesY);
+
+
+            GUILayout.Space(80);
+
+            if (GUILayout.Button("Create tiles"))
             {
                 if (prefab == null)
                 {
-                    Debug.Log("Pls put a prefab on the field");
+                    Debug.Log("Please put a prefab on the field");
                     return;
                 }
+
                 CreatePrefabs();
             }
         }
@@ -41,10 +72,19 @@ namespace Editor
         private void CreatePrefabs()
         {
             var tileFather = new GameObject("TileContainer");
+            var currentPositionX = startPositionX;
+            var currentPositionY = startPositionX;
+            var currentPositionZ = startPositionZ;
             
-            var tile =  Instantiate(prefab);
-            tile.transform.position = Vector3.zero;
-
+            for (int i = 0; i < amountOfTilesX; i++)
+            {
+                for (int j = 0; j < amountOfTilesY; j++)
+                {
+                    var tile = Instantiate(prefab, tileFather.transform, true);
+                    tile.transform.position = new Vector3(currentPositionX + i, currentPositionY, startPositionZ + j);
+                }
+                
+            }
         }
     }
 }
